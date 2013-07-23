cbae2ee00b3bbe54aa8771c01f79630e0e8fbe8b
Andrey Pavlov <couchemar@yandex.ru>
2013-07-23 13:15:51 +0400

    Добавили релизы

--- Makefile
+++ Makefile
@@ -1,8 +1,20 @@
 node1:
-	@iex --erl "-config conf1" -S mix
+	@iex --sname node1 --erl "-config conf1" -S mix
 
 node2:
-	@iex --erl "-config conf2" -S mix
+	@iex --sname node2 --erl "-config conf2" -S mix
 
 node3:
-	@iex --erl "-config conf3" -S mix
+	@iex --sname node3 --erl "-config conf3" -S mix
+
+release:
+	@mix relex.assemble
+
+rel1:
+	./rel/kvs/bin/iex --sname node1 --erl "-config conf1"
+
+rel2:
+	./rel/kvs/bin/iex --sname node2 --erl "-config conf2"
+
+rel3:
+	./rel/kvs/bin/iex --sname node3 --erl "-config conf3"
--- mix.exs
+++ mix.exs
@@ -1,10 +1,13 @@
+Code.append_path "deps/relex/ebin"
+
 defmodule Kvs.Mixfile do
   use Mix.Project
 
   def project do
     [ app: :kvs,
       version: "0.0.1",
-      deps: deps ]
+      deps: deps,
+      release_options: [path: "rel"] ]
   end
 
   # Configuration for the OTP application
@@ -13,9 +16,9 @@ defmodule Kvs.Mixfile do
       applications: [:ranch],
       mod: {Kvs, []},
       env: [ listen_port: 8090,
-             nodes: [ :"node1@couchemar",
-                      :"node2@couchemar",
-                      :"node3@couchemar" ]
+             nodes: [ :"node1@couchemar-P5KR",
+                      :"node2@couchemar-P5KR",
+                      :"node3@couchemar-P5KR" ]
            ] ]
   end
 
@@ -24,6 +27,19 @@ defmodule Kvs.Mixfile do
   defp deps do
     [{:ranch, git: "https://github.com/extend/ranch.git", tag: "0.8.3"},
      {:luerl, git: "https://github.com/rvirding/luerl.git", branch: "new-engine",
-      compile: "make && cp src/luerl.app.src ebin/luerl.app"}]
+      compile: "make && cp src/luerl.app.src ebin/luerl.app"},
+     {:relex, github: "yrashk/relex"}]
   end
+
+  if Code.ensure_loaded?(Relex.Release) do
+    defmodule Release do
+      use Relex.Release
+
+      def name, do: atom_to_binary(Mix.project()[:app], :utf8)
+      def version, do: Mix.project[:version]
+      def applications, do: [Mix.project()[:app], :luerl]
+      def lib_dirs, do: ["deps"]
+    end
+  end
+
 end
